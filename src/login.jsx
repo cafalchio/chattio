008931e4b270a458c7363d8f20fb6e4df9d7759e
cafalchio <mcafalchio@gmail.com>
2025-01-03 12:01:45 +0000

    updated logos and use login component

--- src/login.jsx
+++ src/login.jsx
@@ -1,77 +1,120 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import { Button, Checkbox, Form, Grid, Input, theme, Typography } from "antd";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
 
-function Login(props) {
-    const { setUserName, setLogged, logged, setUsers } = props;
-    const [error, setError] = useState(null);
+const { useToken } = theme;
+const { useBreakpoint } = Grid;
+const { Text, Title, Link } = Typography;
 
-    function getUsers() {
-        fetch("http://192.168.0.9:8009/users", {
-            method: "GET",
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setUsers(data);
-            });
+export default function App() {
+  const { token } = useToken();
+  const screens = useBreakpoint();
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  const styles = {
+    container: {
+      margin: "0 auto",
+      padding: screens.md ? `${token.paddingXL}px` : `${token.sizeXXL}px ${token.padding}px`,
+      width: "380px"
+    },
+    footer: {
+      marginTop: token.marginLG,
+      textAlign: "center",
+      width: "100%"
+    },
+    forgotPassword: {
+      float: "right"
+    },
+    header: {
+      marginBottom: token.marginXL
+    },
+    section: {
+      alignItems: "center",
+      backgroundColor: token.colorBgContainer,
+      display: "flex",
+      height: screens.sm ? "100vh" : "auto",
+      padding: screens.md ? `${token.sizeXXL}px 0px` : "0px"
+    },
+    text: {
+      color: token.colorTextSecondary
+    },
+    title: {
+      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3
     }
+  };
 
-    useEffect(() => {
-        getUsers();
-    }, [logged]);
-
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        fetch("http://192.168.0.9:8009/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ user: user }),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    setLogged(true);
-
-                } else if (response.status === 401) {
-                    setError("Invalid username");
-                }
-            })
-            .catch((error) => {
-                setError("Could not login, try later");
-            });
-    }
-
-
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-inherit">
-            <div className=" p-8 rounded-lg max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                <form>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            Username
-                        </label>
-                        <input
-                            type="text"
-                            id="username"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            onChange={(e) => setUserName(e.target.value)}
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button
-                            onClick={handleSubmit}
-                            type="submit"
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        >
-                            Sign In
-                        </button>
-                    </div>
-                    {error && <p className="text-red-500 text-xs italic">{error}</p>}
-                </form>
-            </div>
+  return (
+    <section style={styles.section}>
+      <div style={styles.container}>
+        <div style={styles.header}>
+          chattio <img src="src/assets/32x32.png" width={"22px"}/>
+          <Title style={styles.title}>Sign in</Title>
+          <Text style={styles.text}>
+            Welcome back to Chattio App UI! Please enter your details below to
+            sign in.
+          </Text>
         </div>
-    );
-};
-
-export default Login;
+        <Form
+          name="normal_login"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          layout="vertical"
+          requiredMark="optional"
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <a style={styles.forgotPassword} href="">
+              Forgot password?
+            </a>
+          </Form.Item>
+          <Form.Item style={{ marginBottom: "0px" }}>
+            <Button block="true" type="primary" htmlType="submit">
+              Log in
+            </Button>
+            <div style={styles.footer}>
+              <Text style={styles.text}>Don't have an account?</Text>{" "}
+              <Link href="">Sign up now</Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  important:true,
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
     "../src-tauri/**/*.{html,js}",
